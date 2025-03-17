69e5fca112fb5a67b185e1c04c378ce415d2eda0
MistyNine <sakorn.k@ku.th>
2025-03-17 16:51:51 +0700

    edit รอบแรก

--- 241-2567-700/user.js
+++ 241-2567-700/user.js
@@ -1,42 +1,51 @@
 const BASE_URL = 'http://localhost:8000'
 window.onload = async () => {
-    await lodeData()
+    await loadData()
 }
-const lodeData = async () => {
-    console.log('user page loaded')
-    // 1. load user ทั้งหมด จาก api ที่เตรียมไว้
-    const response = await axios.get(`${BASE_URL}/user`)
 
-    console.log(response.data)
-
+const loadData = async () => {
+    console.log('user page onload')
+    //1. load all user จาก API ที่เราสร้างไว้
+    const response = await axios.get (`${BASE_URL}/users`)
+    console.log( response.data )
 
     const userDOM = document.getElementById('user')
-    // 2. นำ user ทั้งหมด โหลดกลับเข้าไปใน html *แสดงเข้าไปที่ html*
+    //2.นำ user ทั้งหมด load กลับเข้าไป html 
     
     let htmlData = '<div>'
-    for (let i = 0; i < response.data.length; i++) {
-        let user = response.data[i]
-        htmlData += `<div>
-        ${user.id} ${user.firstname} ${user.lastname}
-        <a href='index.html?id=${user.id}'> <button>Edit</button></a>
-        <button class = 'delete' data-id = '${user.id}'>delete</button>
-    </div>`
+    for(let i = 0; i < response.data.length; i++){
+    let user = response.data[i]
+    htmlData += `<tr> 
+        <td>${user.id}</td>
+        <td>${user.firstname}</td>
+        <td>${user.lastname}</td>
+        <td>${user.age}</td>
+        <td>${user.gender}</td>
+        <td>${user.interests}</td>
+        <td>${user.description}</td>
+        <td>
+            <a href = 'index.html?id=${user.id}'><button>Edit</button></a>        
+            <button class='delete' data-id='${user.id}'>Delete</button>
+        </td>
+    </tr>  
+    `;
     }
-    htmlData += '</div>'
+    htmlData += `</div >`
     userDOM.innerHTML = htmlData
 
-    // 3. delete user
-    const deleteDOMs = document.getElementsByClassName('delete')
-    for (let i = 0; i < deleteDOMs.length; i++) {
-        deleteDOMs[i].addEventListener('click', async (event) => {
-            // ดึง id ของ user ที่ต้องการลบ
-            const id = event.target.dataset.id
-            try {
-                await axios.delete(`${BASE_URL}/user/${id}`)
-                lodeData()
-            } catch (error) {
-                console.error('error',error)
-            }
-        })
-    }
+    //3. ให้ปุ่ม Delete ทำงาน
+    const deleteDOM = document.getElementsByClassName('delete')
+    for(let i = 0; i < deleteDOM.length; i++){
+    deleteDOM[i].addEventListener('click', async (event) => {
+        //ดึง id ของ user ที่ต้องการลบ
+        const id =  event.target.dataset.id
+        try{
+            await axios.delete(`${BASE_URL}/users/${id}`)
+            loadData()// recusive function คือ การเรียก function ตัวเอง 
+        }catch(error){
+            console.log('error',error);
+            
+      }
+    })
+}
 }
